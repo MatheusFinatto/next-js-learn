329ad99e55e7ba78d0c735ce71f0f6d0b514fdfe
MatheusFinatto <matheusfinatto2@gmail.com>
2023-01-24 16:23:43 -0300

    fix: i really couldnt change the title in the last two tries

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "[Your Name]";
+const name = "Matheus Finatto";
 export const siteTitle = "Next.js Sample Website - Matheus Finatto's version";
 
 export default function Layout({ children, home }) {
